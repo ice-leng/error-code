b56d8037093395abe651faf7e1700a473339f24d
冷斌 <xykxyk2008@163.com>
2022-03-17 22:13:02 +0800

    add enumview

---
+++ src/Annotation/EnumView.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Date:  2022/3/17
+ * Time:  10:10 PM
+ */
+
+declare(strict_types=1);
+
+namespace Lengbin\ErrorCode\Annotation;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+class EnumView
+{
+    public const ENUM_MESSAGE = 1;
+    public const ENUM_VALUE = 2;
+    public const ENUM_ALL = 3;
+
+    public int $flags;
+
+    public function __construct(int $flags = self::ENUM_ALL)
+    {
+        $this->flags = $flags;
+    }
+}
